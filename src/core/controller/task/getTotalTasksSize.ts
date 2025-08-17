ac0143f32f9a01c236b2a963348911cc15c7abf0
lazloalexandru <lazloalexandru@gmail.com>
2025-08-17 23:54:25 +0300

    removed checkpoint remains

--- src/core/controller/task/getTotalTasksSize.ts
+++ src/core/controller/task/getTotalTasksSize.ts
@@ -3,7 +3,7 @@ import { EmptyRequest, Int64 } from "@shared/proto/cline/common"
 import { getTotalTasksSize as calculateTotalTasksSize } from "../../../utils/storage"
 
 /**
- * Gets the total size of all tasks including task data and checkpoints
+ * Gets the total size of all tasks including task data
  * @param controller The controller instance
  * @param _request The empty request
  * @returns The total size as an Int64 value
